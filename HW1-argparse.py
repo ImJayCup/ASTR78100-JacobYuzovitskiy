9ca6d4440642cd6bf8ce3255322851ec732c6793
Jacob Yuzovitskiy <140641226+ImJayCup@users.noreply.github.com>
2025-09-03 21:33:22 -0400

    Update HW1-argparse.py
    Added rounding and a nicer printout for the function. Added program description to argparse.

--- HW1-argparse.py
+++ HW1-argparse.py
@@ -1,10 +1,10 @@
 import sys
 import argparse
-parser = argparse.ArgumentParser(description="Do Something")
+parser = argparse.ArgumentParser(description="Calculates the time it takes for a ball to hit the ground with a given height and acceleration due to gravity.")
 parser.add_argument("h",type=float,help="Height of the Ball off the ground")
 parser.add_argument("g",type=float,default=9.81,help="Strength of Gravity on target planet")
 args=parser.parse_args()
 def hitTheGround(h,g=9.81):
     t = (2*g/h)**0.5
     return t
-print(hitTheGround(args.h,g=args.g))
+print(f"The ball hit the ground in {hitTheGround(args.h,g=args.g):.2f}s.")
