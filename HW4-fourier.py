7eb0b743766c199d45bdbc4525568500299f1f4f
Jacob Yuzovitskiy <jacob.yuzovitskiy@macaulay.cuny.edu>
2025-10-12 14:35:52 -0400

    Added cleaner plots

--- HW4-fourier.py
+++ HW4-fourier.py
@@ -5,29 +5,18 @@ fluxes = hdul[1].data['fluxes']
 ferrs = hdul[1].data['ferrs']
 
 from matplotlib import pyplot as plt
-plt.scatter(times,fluxes)
-plt.xlim([1683,1690])
-plt.show()
 
-#for every entry in fluxes, if the next entry is more than one timestep away,
-#interpolate the last two points to guess the next point
+# Number of points to use
+n_pts = 250
 
+# Interpolate to uniform spacing for FFT
 import numpy as np
 from scipy.interpolate import interp1d
-import matplotlib.pyplot as plt
-
-# Create uniform grid
-x_uniform = np.linspace(times[:100].min(), times[:100].max())
+x_uniform = np.linspace(times[:n_pts].min(), times[:n_pts].max(), num=len(times[:n_pts]))
 
 # Interpolate
-interp_func = interp1d(times[:100], fluxes[:100], kind='cubic')
+interp_func = interp1d(times[:n_pts], fluxes[:n_pts], kind='cubic')
 y_uniform = interp_func(x_uniform)
-
-# Plot to check
-plt.plot(times[:100], fluxes[:100], 'o', label='Original')
-plt.plot(x_uniform, y_uniform, '-', label='Interpolated')
-plt.legend()
-plt.show()
 
 # Now y_uniform is ready for FFT
 N = len(y_uniform)
@@ -37,9 +26,22 @@ frequencies = np.fft.fftfreq(N, dt)
 
 # Removing the 0.0 frequency component
 fft_result[0] = 0
-plt.plot(frequencies[:N//2], np.abs(fft_result)[:N//2])  # Plot only positive frequencies
-plt.xlabel('Frequency')
-plt.ylabel('Amplitude')
-plt.title('FFT of Interpolated Signal (0 freq removed)')
+fig, axs = plt.subplots(4, 1, figsize=(10, 8))  # no sharex
+
+# time-domain panels
+axs[0].scatter(times, fluxes, s=5)
+axs[0].set_xlim(1683, 1690)
+
+axs[1].plot(times[:n_pts], fluxes[:n_pts], 'o', label='Original')
+axs[1].plot(x_uniform, y_uniform, '-', label='Interpolated')
+axs[1].set_xlim(1683, 1690)
+axs[1].legend()
+
+# frequency-domain panel
+axs[2].plot(frequencies[:N//2], np.abs(fft_result)[:N//2])
+axs[2].set_xlabel('Frequency')
+
+plt.tight_layout()
 plt.show()
+
 
