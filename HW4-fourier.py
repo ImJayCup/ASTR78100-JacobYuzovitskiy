2989bd59b49e838c575a3e0d95ef783f7e6e90db
Jacob Yuzovitskiy <jacob.yuzovitskiy@macaulay.cuny.edu>
2025-10-12 14:04:48 -0400

    Simple working fft

--- HW4-fourier.py
+++ HW4-fourier.py
@@ -7,22 +7,39 @@ ferrs = hdul[1].data['ferrs']
 from matplotlib import pyplot as plt
 plt.scatter(times,fluxes)
 plt.xlim([1683,1690])
-#plt.show()
+plt.show()
 
 #for every entry in fluxes, if the next entry is more than one timestep away,
 #interpolate the last two points to guess the next point
 
+import numpy as np
+from scipy.interpolate import interp1d
+import matplotlib.pyplot as plt
 
+# Create uniform grid
+x_uniform = np.linspace(times[:100].min(), times[:100].max())
 
-from numpy import zeros,exp,pi
-def dft(y):
-    N = len(y)
-    c = zeros(N//2+1,complex)
-    for k in range(N//2+1):
-        for n in range(N):
-            c[k] += y[n]*exp(-2j*pi*k*n/N)
-    return c
+# Interpolate
+interp_func = interp1d(times[:100], fluxes[:100], kind='cubic')
+y_uniform = interp_func(x_uniform)
 
-#c = dft(fluxes[:200])
-#plt.plot(abs(c))
-#plt.show()
+# Plot to check
+plt.plot(times[:100], fluxes[:100], 'o', label='Original')
+plt.plot(x_uniform, y_uniform, '-', label='Interpolated')
+plt.legend()
+plt.show()
+
+# Now y_uniform is ready for FFT
+N = len(y_uniform)
+dt = x_uniform[1] - x_uniform[0]  # Assuming uniform spacing
+fft_result = np.fft.fft(y_uniform)
+frequencies = np.fft.fftfreq(N, dt)
+
+# Removing the 0.0 frequency component
+fft_result[0] = 0
+plt.plot(frequencies[:N//2], np.abs(fft_result)[:N//2])  # Plot only positive frequencies
+plt.xlabel('Frequency')
+plt.ylabel('Amplitude')
+plt.title('FFT of Interpolated Signal (0 freq removed)')
+plt.show()
+
